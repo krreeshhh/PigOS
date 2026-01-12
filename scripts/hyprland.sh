b354f222f328c595753c8525e9115fd883fe2386
tokittoo <krishnaprasath.7x@gmail.com>
2026-01-12 09:25:19 +0530

    I think it fixed

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -11,14 +11,14 @@ print_info "\nStarting hyprland setup..."
 print_info "\nEverything is recommended to INSTALL"
 
 run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
-run_command "bash -c 'mkdir -p \"/home/$SUDO_USER/.config/hypr/\" && cp -r \"$BASE_DIR/configs/hypr/hyprland.conf\" \"/home/$SUDO_USER/.config/hypr/\"'" "Copy hyprland config (Must)" "yes" "no" 
+run_command "mkdir -p /home/$SUDO_USER/.config/hypr/ && cp -r $BASE_DIR/configs/hypr/hyprland.conf /home/$SUDO_USER/.config/hypr/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/hypr/hyprland.conf" "Copy hyprland config (Must)" "yes" 
 
 run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
 
 run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
 
 run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
-run_command "cp -r $BASE_DIR/configs/dunst /home/$SUDO_USER/.config/" "Copy dunst config" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/dunst /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/dunst" "Copy dunst config" "yes"
 
 run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
 
--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -10,27 +10,27 @@ log_message "Installation started for utilities section"
 print_info "\nStarting utilities setup..."
 
 run_command "pacman -S --noconfirm waybar" "Install Waybar - Status Bar" "yes"
-run_command "cp -r $BASE_DIR/configs/waybar /home/$SUDO_USER/.config/" "Copy Waybar config" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/waybar /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/waybar" "Copy Waybar config" "yes"
 
 run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
-run_command "cp -r $BASE_DIR/configs/tofi /home/$SUDO_USER/.config/" "Copy Tofi config(s)" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/tofi /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/tofi" "Copy Tofi config(s)" "yes"
 
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
 
 run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper management" "yes" "no"
-run_command "bash -c 'mkdir -p \"/home/$SUDO_USER/.config/assets/backgrounds\" && cp -r \"$BASE_DIR/configs/assets/backgrounds\" \"/home/$SUDO_USER/.config/assets/\"'" "Copy sample wallpapers to assets directory (Recommended)" "yes" "no"
+run_command "mkdir -p /home/$SUDO_USER/.config/assets/backgrounds && cp -r $BASE_DIR/configs/assets/backgrounds /home/$SUDO_USER/.config/assets/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/assets" "Copy sample wallpapers to assets directory (Recommended)" "yes"
 
 run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Color Picker" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen Locker (Must)" "yes" "no"
-run_command "cp -r $BASE_DIR/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Copy Hyprlock config" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/hypr/hyprlock.conf" "Copy Hyprlock config" "yes"
 
 run_command "yay -S --sudoloop --noconfirm wlogout" "Install Wlogout - Session Manager" "yes" "no"
-run_command "cp -r $BASE_DIR/configs/wlogout /home/$SUDO_USER/.config/" "Copy Wlogout config and assets" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/wlogout /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/wlogout" "Copy Wlogout config and assets" "yes"
 
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hypridle" "Install Hypridle for idle management (Must)" "yes" "no"
-run_command "cp -r $BASE_DIR/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/" "Copy Hypridle config" "yes" "no"
+run_command "cp -r $BASE_DIR/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/hypr/hypridle.conf" "Copy Hypridle config" "yes"
 
 echo "------------------------------------------------------------------------"
